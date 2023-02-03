6ab5af6fcdf8d23f1301ec4ca8cd2be7bf046748
Daniel Murdolo <dmurdolo@gmail.com>
2023-02-03 22:27:36 +0000

    Add camera setup and controls, define map and boundaries

---
+++ TheGame/scripts/cameraControl.gd
@@ -0,0 +1,78 @@
+extends Spatial
+
+export var SCROLL_SPEED:float = 10
+export var ZOOM_SPEED:float = 2
+
+export var MAP_BOUNDARY:int = 40
+
+var UP:bool = false
+var DOWN:bool = false
+var LEFT:bool = false
+var RIGHT:bool = false
+
+func outsideMapBoundary(position, modifier:int = 0):
+	return position < (MAP_BOUNDARY - modifier)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		# rotation
+		if event.button_mask&(BUTTON_MASK_MIDDLE+BUTTON_MASK_RIGHT):
+			self.rotate(Vector3(0, 1, 0), event.relative.x * -0.002)
+
+		# scroll x
+		if event.position.x<=5:
+			LEFT=true
+		elif event.position.x>OS.window_size.x-5:
+			RIGHT=true
+		else:
+			LEFT=false
+			RIGHT=false
+
+		#scroll y
+		if event.position.y<=5:
+			UP=true
+		elif event.position.y>OS.window_size.y-5:
+			DOWN=true
+		else:
+			UP=false
+			DOWN=false
+
+	# zoom
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			# zoom in
+			if event.button_index == BUTTON_WHEEL_DOWN:
+				if $Camera.fov<80:
+					$Camera.fov+=ZOOM_SPEED
+			# zoom out
+			if event.button_index == BUTTON_WHEEL_UP:
+				if $Camera.fov>50:
+					$Camera.fov-=ZOOM_SPEED
+
+func _process(delta):
+	if Input.is_key_pressed(KEY_W) or UP:
+		if outsideMapBoundary(-self.transform.origin.z):
+			self.translate(Vector3(0,0,-SCROLL_SPEED*delta))
+
+	if Input.is_key_pressed(KEY_S) or DOWN:
+		if outsideMapBoundary(self.transform.origin.z, 5):
+			self.translate(Vector3(0,0,SCROLL_SPEED*delta))
+
+	if Input.is_key_pressed(KEY_A) or LEFT:
+		if outsideMapBoundary(-self.transform.origin.x):
+			self.translate(Vector3(-SCROLL_SPEED*delta,0,0))
+
+	if Input.is_key_pressed(KEY_D) or RIGHT:
+		if outsideMapBoundary(self.transform.origin.x, 5):
+			self.translate(Vector3(SCROLL_SPEED*delta,0,0))
+
+	if Input.is_key_pressed(KEY_Q):
+		self.rotate(Vector3(0,1,0),1*delta)
+
+	if Input.is_key_pressed(KEY_E):
+		self.rotate(Vector3(0,1,0),-1*delta)
+
