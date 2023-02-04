e94d8a0e7ed1439070b0ec5c49a71af8e5f57218
Daniel Murdolo <dmurdolo@gmail.com>
2023-02-04 01:16:56 +0000

    Update camera zoom and parameters, add gitignore

--- TheGame/scripts/cameraControl.gd
+++ TheGame/scripts/cameraControl.gd
@@ -3,6 +3,9 @@ extends Spatial
 export var SCROLL_SPEED:float = 10
 export var ZOOM_SPEED:float = 2
 
+export var ZOOM_MIN:float = 5
+export var ZOOM_MAX:float = 18
+
 export var MAP_BOUNDARY:int = 40
 
 var UP:bool = false
@@ -44,22 +47,22 @@ func _input(event):
 	# zoom
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			# zoom in
-			if event.button_index == BUTTON_WHEEL_DOWN:
-				if $Camera.fov<80:
-					$Camera.fov+=ZOOM_SPEED
 			# zoom out
-			if event.button_index == BUTTON_WHEEL_UP:
-				if $Camera.fov>50:
-					$Camera.fov-=ZOOM_SPEED
+			if event.button_index == BUTTON_WHEEL_DOWN:
+				if self.transform.origin.y < ZOOM_MAX:
+					self.translate(Vector3(0, ZOOM_SPEED, 0))
+			# zoom in
+			elif event.button_index == BUTTON_WHEEL_UP:
+				if self.transform.origin.y > ZOOM_MIN:
+					self.translate(Vector3(0, -ZOOM_SPEED, 0))
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_W) or UP:
-		if outsideMapBoundary(-self.transform.origin.z):
+		if outsideMapBoundary(-self.transform.origin.z, 20):
 			self.translate(Vector3(0,0,-SCROLL_SPEED*delta))
 
 	if Input.is_key_pressed(KEY_S) or DOWN:
-		if outsideMapBoundary(self.transform.origin.z, 5):
+		if outsideMapBoundary(self.transform.origin.z, -35):
 			self.translate(Vector3(0,0,SCROLL_SPEED*delta))
 
 	if Input.is_key_pressed(KEY_A) or LEFT:
