868c238dc8edf8325b3d7396c67de0c7472b9b77
Daniel Murdolo <dmurdolo@gmail.com>
2023-02-05 00:06:26 +0000

    Use enemy spawn points to instantiate enemies

--- TheGame/scripts/GameManager.gd
+++ TheGame/scripts/GameManager.gd
@@ -4,15 +4,14 @@ export (PackedScene) var mob_prefab
 
 export var DEBUG_SPAWN = false
 
-export var enemySpawns = []
-
 var current_energy:int = 10
 var max_energy:int = 10
 var blue_tower = null
 var red_tower = null
 var blue_path = []
 var red_path = []
-var spawns_per_wave = 10
+var spawns_per_wave:int = 5
+var wave:int = 0
 
 #Power Nodes
 var test_nodes
@@ -28,12 +27,12 @@ func _ready():
 		_map_edge(tower, tower.next_tower_1, tower_edge_map)
 		_map_edge(tower, tower.next_tower_2, tower_edge_map)
 		_map_edge(tower, tower.next_tower_3, tower_edge_map)
-		if tower.team_spawn == "blue":
-			blue_tower = tower
+		# if tower.team_spawn == "blue":
+		# 	blue_tower = tower
 		if tower.team_spawn == "red":
 			red_tower = tower
-	if blue_tower:
-		blue_path = _generate_path(blue_tower, blue_tower, tower_edge_map, {}, true)
+	# if blue_tower:
+	# 	blue_path = _generate_path(blue_tower, blue_tower, tower_edge_map, {}, true)
 	if red_tower:
 		red_path = _generate_path(red_tower, red_tower, tower_edge_map, {}, true)
 	if (DEBUG_SPAWN):
@@ -97,16 +96,20 @@ func do_spawn():
 	var teams = []
 	if red_path:
 		teams.append({ "team": "red", "enemy_team": "blue", "enemy_tower": blue_tower, "path": red_path })
-	if blue_path:
-		teams.append({ "team": "blue", "enemy_team": "red", "enemy_tower": red_tower, "path": blue_path })
+	# if blue_path:
+	# 	teams.append({ "team": "blue", "enemy_team": "red", "enemy_tower": red_tower, "path": blue_path })
 	for team_data in teams:
 		var _team = team_data["team"]
 		var path = team_data["path"]
+		
+		var spawns = get_node("/root/Level/EnemySpawnNodes");
+		var spawnIndex = randi() % spawns.get_child_count()
+		var spawn = spawns.get_child(spawnIndex)
 
 		for _i in range(spawns_per_wave):
 			var mob = mob_prefab.instance()
 			add_child(mob)
-			mob.initialize(path, team_data["team"], team_data["enemy_team"])
+			mob.initialize(spawn, path, team_data["team"], team_data["enemy_team"])
 
 
 func _on_Timer_timeout():
